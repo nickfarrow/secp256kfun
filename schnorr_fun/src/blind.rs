6b5a8b49dec0efa7708da3f46e0d70e7f4dd3f12
nickfarrow <nick@nickfarrow.com>
2022-06-16 10:55:04 +1000

    almost working test

--- schnorr_fun/src/blind.rs
+++ schnorr_fun/src/blind.rs
@@ -1,46 +1,49 @@
+#![allow(missing_docs)]
 //! Blind Schnorr Signatures
 //!
 //! Folllowing https://suredbits.com/schnorr-applications-blind-signatures/
 //!
 
 use crate::fun::rand_core::{CryptoRng, RngCore};
-use crate::{adaptor::EncryptedSignature, musig::Nonce, Message, Schnorr, Signature, Vec};
-use secp256kfun::digest::{Reset, Update};
+use crate::{Message, Schnorr};
 use secp256kfun::{
-    derive_nonce,
     digest::{generic_array::typenum::U32, Digest},
     g,
-    hash::{HashAdd, Tagged},
     marker::*,
-    nonce::NonceGen,
-    s, Point, Scalar, XOnly, G,
+    s, Point, Scalar, G,
 };
 
 pub fn create_blindings<'a, H: Digest<OutputSize = U32> + Clone, NG, R: RngCore + CryptoRng>(
-    nonce: XOnly,
-    public_key: XOnly,
+    nonce: Point,
+    public_key: Point,
     message: Message,
     schnorr: Schnorr<H, NG>,
     rng: &mut R,
-) -> (XOnly, XOnly, Scalar, Scalar, Scalar, Scalar) {
+) -> (Point, Point, Scalar, Scalar, Scalar, Scalar) {
     let alpha = Scalar::random(rng);
     let t = Scalar::random(rng);
     let beta = Scalar::random(rng);
 
-    let blinded_public_key = g!({ public_key.to_point() } + t * G)
+    // rename blinded to tweaked
+    let blinded_public_key = g!({ public_key } + t * G)
         .normalize()
         .mark::<NonZero>()
-        .expect("added tweak is random")
-        .to_xonly();
-    let blinded_nonce = g!({ nonce.to_point() } + alpha * G)
+        .expect("added tweak is random");
+
+    let blinded_nonce = g!(nonce + alpha * G + beta * public_key)
         .normalize()
         .mark::<NonZero>()
-        .expect("added tweak is random")
-        .to_xonly();
-    let blinded_challenge =
-        s!({ schnorr.challenge(blinded_nonce, blinded_public_key, message) } + beta)
-            .mark::<NonZero>()
-            .expect("added tweak is random");
+        .expect("added tweak is random");
+
+    let blinded_challenge = s!({
+        schnorr.challenge(
+            blinded_nonce.to_xonly(),
+            blinded_public_key.to_xonly(),
+            message,
+        )
+    } + beta)
+    .mark::<NonZero>()
+    .expect("added tweak is random");
     (
         blinded_public_key,
         blinded_nonce,
@@ -51,60 +54,51 @@ pub fn create_blindings<'a, H: Digest<OutputSize = U32> + Clone, NG, R: RngCore 
     )
 }
 
+#[derive(Debug)]
 pub struct Blinder {
-    blinded_nonce: XOnly,
-    blinded_public_key: XOnly,
-    blinded_challenge: Scalar,
-    // message: Message<'a>,
+    pubnonce: Point,
+    public_key: Point,
+    challenge: Scalar,
     alpha: Scalar,
     beta: Scalar,
     t: Scalar,
-    unblinded_nonce: XOnly,
-    unblinded_public_key: XOnly,
 }
 
 impl Blinder {
     pub fn blind<H: Digest<OutputSize = U32> + Clone, R: RngCore + CryptoRng>(
-        nonce: XOnly,
-        public_key: XOnly,
+        pubnonce: Point,
+        public_key: Point,
         message: Message,
         schnorr: Schnorr<H>,
         rng: &mut R,
     ) -> Self {
         let (blinded_public_key, blinded_nonce, blinded_challenge, alpha, t, beta) =
-            create_blindings(nonce, public_key, message, schnorr, rng);
+            create_blindings(pubnonce, public_key, message, schnorr, rng);
 
         Blinder {
-            blinded_challenge,
-            blinded_nonce,
-            blinded_public_key,
+            public_key: blinded_public_key,
+            pubnonce: blinded_nonce,
+            challenge: blinded_challenge,
             alpha,
-            beta,
             t,
-            unblinded_nonce: nonce,
-            unblinded_public_key: public_key,
+            beta,
         }
+    }
+
+    pub fn unblind(&self, blinded_signature: Scalar<Public, Zero>) -> Scalar<Public, Zero> {
+        s!(blinded_signature + self.alpha).mark::<Public>()
     }
 }
 
-pub struct Unblinder {}
+pub fn blind_sign(secret: &Scalar, nonce: Scalar, blind_challenge: Scalar) -> Scalar<Public, Zero> {
+    s!(nonce + blind_challenge * secret).mark::<Public>()
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::fun::rand_core::{CryptoRng, RngCore};
-    use crate::{adaptor::EncryptedSignature, musig::Nonce, Message, Schnorr, Signature, Vec};
-    use secp256kfun::digest::{Reset, Update};
-    use secp256kfun::nonce::Deterministic;
-    use secp256kfun::{
-        derive_nonce,
-        digest::{generic_array::typenum::U32, Digest},
-        g,
-        hash::{HashAdd, Tagged},
-        marker::*,
-        nonce::NonceGen,
-        s, Point, Scalar, XOnly, G,
-    };
+    use crate::{Message, Schnorr, Signature};
+    use secp256kfun::{g, Scalar, G};
     use sha2::Sha256;
 
     // proptest! {
@@ -114,23 +108,41 @@ mod test {
 
     #[test]
     fn test_blind_unblind() {
-        // let schnorr = crate::test_instance!();
-        let schnorr = Schnorr::<Sha256, _>::new(Deterministic::<Sha256>::default())
-        let keypair = schnorr.new_keypair(Scalar::random(&mut rand::thread_rng()));
+        let schnorr = Schnorr::<Sha256, _>::new(());
+        // This is the secret & public key of the party blindly signing
+        let secret = Scalar::random(&mut rand::thread_rng());
+        let public_key = g!(secret * G).normalize();
 
         // Probably want to reintroduce a singular nonce struct? And move musig/frost to "binonce"
         let nonce = Scalar::random(&mut rand::thread_rng());
-        let pub_nonce = g!(nonce * G).normalize().to_xonly();
+        // The blind signer sends a nonce public nonce
+        let pub_nonce = g!(nonce * G).normalize();
 
         let message =
             Message::<Public>::plain("test", b"Chancellor on brink of second bailout for banks");
 
+        // we calculate a blinded challenge and send to the signer
         let blinded = Blinder::blind(
             pub_nonce,
-            keypair.public_key(),
+            public_key,
             message,
-            schnorr,
+            schnorr.clone(),
             &mut rand::thread_rng(),
         );
+
+        // blind signer uses this challenge to sign under their secret key
+        let blind_signature = blind_sign(&secret, nonce, blinded.challenge.clone());
+
+        // we recieve the blinded signature from the signer, and unblind it
+        let unblinded_signature = Signature {
+            s: blinded.unblind(blind_signature),
+            R: blinded.pubnonce.to_xonly(),
+        };
+
+        dbg!(&blinded);
+        dbg!(&unblinded_signature);
+        // This signature is valid under the signer's public key
+        let (verification_public_key, negated) = public_key.into_point_with_even_y();
+        assert!(schnorr.verify(&verification_public_key, message, &unblinded_signature));
     }
 }
